921b35326814313800da832d8f97bd3dd3ed8158
joy4563 <joymondal4563@gmail.com>
2024-05-17 23:54:51 +0600

    W-8 D-7

---
+++ Week-8/D-7/A_Maximum_GCD.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define nl cout << '\n';
+
+int main()
+{
+    int test;
+    cin >> test;
+    for (int t = 0; t < test; t++)
+    {
+        int n;
+        cin >> n;
+        cout << n / 2 << endl;
+    }
+
+    return 0;
+}
