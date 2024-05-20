12c298677dfad678e791533b24c8f799b33c990b
joy4563 <joymondal4563@gmail.com>
2024-05-20 23:41:21 +0600

    W-9 D-2

---
+++ Week-9/D-3/Sum_of_Three_Values.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define nl cout << '\n';
+
+int main()
+{
+    int test;
+    cin >> test;
+    for (int t = 0; t < test; t++)
+    {
+        
+    }
+    
+    return 0;
+}
